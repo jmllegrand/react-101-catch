27aac0383d08de909d468d3b04c7e6d376fbaf1a
jmllegrand <jml.legrand@gmail.com>
2016-04-30 19:16:20 +0200

    feat(Order) Display the content of the state's property (order) + calculate the total amount

--- scripts/main.js
+++ scripts/main.js
@@ -49,7 +49,7 @@ var App = React.createClass({
             {Object.keys(this.state.fishes).map(this.renderFish)}
           </ul>
         </div>
-        <Order/>
+        <Order fishes = {this.state.fishes} orderItems = {this.state.order}/>
         <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes}/>
       </div>
     )
@@ -143,10 +143,44 @@ var Header = React.createClass({
 });
 
 var Order = React.createClass({
-  render: function () {
+  displayOrderItem: function(item){
+    var orderItems = this.props.orderItems;
+    var fishes = this.props.fishes;
     return (
-      <div>
-        <p>This is the Order component</p>
+      <li key={item} index={item}>
+        <span>{orderItems[item]} lb(s)</span>
+        <span>{fishes[item].name}</span>
+        <span className="price">{utils.formatPrice(orderItems[item] * fishes[item].price)}</span>
+       </li>
+    )
+  },
+  render: function () {
+    var ordersId = Object.keys(this.props.orderItems);
+    var orderItems = this.props.orderItems;
+    var fishes = this.props.fishes;
+
+    var totalAmount;
+
+    //TODO: handle the 0 & 1 situations within the reduce()
+    if (ordersId.length === 0) {
+      totalAmount = 0
+    } else if (ordersId.length === 1){
+      totalAmount = orderItems[ordersId[0]] * fishes[ordersId[0]].price
+    } else if (ordersId.length > 1){
+       totalAmount = ordersId.reduce(function(previousValue, currentValue, currentIndex){
+         return (previousValue + orderItems[currentValue] * fishes[currentValue].price) ;
+      }, 0);
+    }
+    return (
+      <div className="order-wrap">
+        <h2>Your Order</h2>
+        <ul className="order">
+          {ordersId.map(this.displayOrderItem)}
+          <li className="total">
+            <strong>Total:</strong>
+            {utils.formatPrice(totalAmount)}
+          </li>
+        </ul>
       </div>
     )
   }
